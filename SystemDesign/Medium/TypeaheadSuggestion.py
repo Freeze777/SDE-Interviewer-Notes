99343dc42ce5ce13632e521df0a4f6b4734eb8ef
Francis, Freeze (Agoda) <Freeze.Francis@agoda.com>
2021-05-26 20:21:21 +0700

    typeahead suggestions

---
+++ SystemDesign/Medium/TypeaheadSuggestion.py
@@ -0,0 +1,31 @@
+"""
+ABC.com is planning to build typeahead suggestions to improve its conversions on bookings
+search on its frontend platforms.
+
+Requirements:
+As user starts searching we auto-populate suggestions based on whatever text he/she typed.
+These suggestion should be top 10 most searched phrases in last 1 month ordered by their frequency.
+
+eg:
+User types B in search box
+Suggestions:
+- Bangkok
+- Bangalore
+- Brisbane
+- Best hotels
+- Best discounts
+- Best deals
+.
+.
+.
+"""
+
+"""
+Expectations:
+Clarify functional requirements
+Clarify non functional requirements
+HLD - Architecture
+API design
+LLD
+DB schema
+"""
