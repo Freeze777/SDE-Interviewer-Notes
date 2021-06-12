f8feccc810b55da5d2fe994074404d69f224de98
Francis, Freeze (Agoda) <Freeze.Francis@agoda.com>
2021-06-12 20:04:26 +0700

    basic sdi

--- SystemDesign/Easy/BasicArchitectureBeginners.py
+++ SystemDesign/Easy/BasicArchitectureBeginners.py
@@ -1,24 +1,24 @@
 """
 Given a basic architecture for simple/minimal booking system that supports 1k users today.
 
-Frontend (1 server) ---> Backend Rest API (1 server) ---> SQL Database (1 server)
+Frontend (1 server) <---> Backend Rest API (1 server) <---> SQL Database (1 server)
 
-Frontend - features a static list of 100 hotels
+Frontend - showcases a static list of 100 hotels
 Backend - handles all data needs of FE
 Database - stores all the user, hotel and booking info.
 
 Features supported today
-- make a booking - book a hotel
+- make a booking - book a hotel with checkin and checkout date.
 - see past bookings - all booking made by user in the past.
 - basic user profile - name, email, phone, language, currency etc.
 
-Architectural changes to scale this system for 1 million users?
+Architectural changes to scale this system for 1 million+ users?
 How to guarantee 99.9% uptime?
 """
 
 """
 Challenge more :
-Slow queries?
+Slow db queries?
 How to handle traffic spikes?
 Slow backend?
 Frontend loading very slowly?
@@ -27,11 +27,11 @@ New requirement to track user activity data from frontend. How to handle?
 
 Expectation:
 - horizontal scaling of backend api and frontend with LB.
-- autoscaling for peak traffic
-- caching layer
+- autoscaling for peak traffic by request rates
+- caching layer for slow changing data
 - master-slave db architecture (geo sharded if billion users)
-- db indexing
+- db indexing for queries
 - CDN for static content for frontend.
-- group the traffic to reads and writes (80:20 read-write ratio)
+- group the traffic to reads and writes (80:20 read-write ratio) and optimise these paths.
 - microservice architecture preferred. (user service, booking service)
 """
