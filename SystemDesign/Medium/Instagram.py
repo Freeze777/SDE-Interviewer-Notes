26c5e8ff4fb9d5d4ddd44a051c6444c1629288eb
Freeze Francis <freezefrancis@gmail.com>
2021-07-11 15:50:27 +0700

    IG sdi

---
+++ SystemDesign/Medium/Instagram.py
@@ -0,0 +1,23 @@
+"""
+- user registration and login
+- user can create post
+- user can follow other users
+- user can like, comment on post of users he follow
+- news feed where user can see other users post
+"""
+
+"""
+
+scale : 1 billion users, 400 million active users per day
+
+"""
+
+"""
+sql - user info
+nosql - user feeds, activities
+memcache - caching
+app server
+kafka/rabbit - notifications
+
+"""
+
