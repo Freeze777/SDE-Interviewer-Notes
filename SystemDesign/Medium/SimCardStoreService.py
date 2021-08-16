ae01c2155b50821217f87002ac49398196d9cebd
Freeze Francis <freezefrancis@gmail.com>
2021-08-16 09:00:50 +0700

    sim card store

--- SystemDesign/Medium/SimCardStoreService.py
+++ SystemDesign/Medium/SimCardStoreService.py
@@ -1,8 +1,8 @@
 """
-Design a sim card store system that gives you 3 sim cards with completely random phone numbers on demand.
-- Phone numbers are 10 digit long and the generated numbers should be hard to predict.
+Design a sim card store system that gives you 3 sim cards on demand each of which has a 10 digit long phone number.
+- The generated numbers should be very hard to predict.
+- We need to keep track of the phone numbers that are sold out and also the total unused numbers the system has at any point of time.
 - Phone numbers should be stored very efficiently and retrieval of available phone numbers should be fast.
-- At any point in time we should be able to know how many unused numbers the system has.
 
 """
 
