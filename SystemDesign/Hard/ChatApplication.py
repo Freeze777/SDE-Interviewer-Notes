b3710340f89a934f94eec9a210726a4fa4e681f6
Freeze Francis <freezefrancis@gmail.com>
2022-01-20 13:38:23 +0700

    sdi - chat app

---
+++ SystemDesign/Hard/ChatApplication.py
@@ -0,0 +1,30 @@
+"""
+Design a realtime chat application like whatsapp, fb messenger etc.:
+
+Requirements:
+1) User registration
+2) 1:1 Chat - Text Message Only (Max 100,000 char per text)
+3) Online/Presence Indicator
+Scale :
+- 50 million daily active users
+
+
+
+Extended feature:
+1) Group chat - max 100 people
+2) Push Notifications
+3) Media file - image, document and video
+
+Out of Scope:
+1) E2E encryption
+"""
+
+"""
+Expectations:
+1) bidirectional communication. How its established?
+2) Database choice - NoSQL for messages or SQL?
+3) * Ordering of messages - specially in group
+4) Celebrity problem in presence/online updates
+5) caching.cdn
+
+"""
