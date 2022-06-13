3fc3b5ebda8b244b909fb301a7f630d389e3dc1b
Freeze Francis <freezefrancis@gmail.com>
2022-06-13 18:01:21 +0700

    multi threaded anagram python code

--- Multithreading/Easy/MultiThreadedAnagram.py
+++ Multithreading/Easy/MultiThreadedAnagram.py
@@ -2,35 +2,35 @@ import threading
 from collections import Counter
 
 
-def chunk(word, chunk_size=5):
-    return [word[i:i + chunk_size] for i in range(0, len(word), chunk_size)]
+def get_chunks(content, chunk_size=5):
+    return [content[i:i + chunk_size] for i in range(0, len(content), chunk_size)]
 
 
-def get_letter_count(word1, idx, results_collector):
+def get_letter_frequency(word1, idx, results_collector):
     results_collector[idx] = Counter(word1)
 
 
-def aggregate_counts(results_collector):
-    final_wc = Counter()
-    for wc in results_collector:
-        final_wc += wc
-    return final_wc
+def aggregate_letter_frequency(results_collector):
+    final_counter = Counter()
+    for counter in results_collector:
+        final_counter += counter
+    return final_counter
 
 
-def compute(content):
-    chunks = chunk(content)
+def compute_letter_frequency(content):
+    chunks = get_chunks(content)
     jobs = [None] * len(chunks)
     results = [None] * len(chunks)
     for chunk_id in range(len(chunks)):
-        t = threading.Thread(target=get_letter_count, args=(chunks[chunk_id], chunk_id, results,))
+        t = threading.Thread(target=get_letter_frequency, args=(chunks[chunk_id], chunk_id, results,))
         jobs[chunk_id] = t
     [j.start() for j in jobs]
     [j.join() for j in jobs]
-    return aggregate_counts(results)
+    return aggregate_letter_frequency(results)
 
 
 def is_anagram(content1, content2):
-    return compute(content1) == compute(content2)
+    return compute_letter_frequency(content1) == compute_letter_frequency(content2)
 
 
 if __name__ == '__main__':
