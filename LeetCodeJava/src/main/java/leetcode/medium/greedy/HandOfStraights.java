9d37d033151f1d414ee08f20dce924132f6b786f
Freeze Francis <freezefrancis@gmail.com>
2022-10-02 11:36:49 +0700

    hand of straights

---
+++ LeetCodeJava/src/main/java/leetcode/medium/greedy/HandOfStraights.java
@@ -0,0 +1,36 @@
+package leetcode.medium.greedy;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * <a href="https://leetcode.com/problems/hand-of-straights/">...</a>
+ */
+public class HandOfStraights {
+
+    public boolean isNStraightHand(int[] hands, int groupSize) {
+        Map<Integer, Integer> map = new TreeMap<>();
+        for (Integer hand : hands) {
+            map.compute(hand, (k, v) -> v == null ? 1 : v + 1);
+        }
+        for (Integer currentHand : map.keySet()) {
+            Integer numCards = map.get(currentHand);
+            if (numCards == 0) continue;
+            for (int i = 1; i < groupSize; i++) {
+                Integer nextHand = currentHand + i;
+                if (!map.containsKey(nextHand)) return false;
+                Integer numNextHandCards = map.get(nextHand);
+                if (numNextHandCards < numCards) return false;
+                map.put(nextHand, numNextHandCards - numCards);
+            }
+            map.put(currentHand, 0);
+        }
+        return map.values().stream().allMatch(v -> v == 0);
+    }
+
+    public static void main(String[] args) {
+        HandOfStraights hos = new HandOfStraights();
+        System.out.println(hos.isNStraightHand(new int[]{1, 2, 3, 6, 2, 3, 4, 7, 8}, 3));
+        System.out.println(hos.isNStraightHand(new int[]{1, 2, 3, 4, 5}, 4));
+    }
+}
