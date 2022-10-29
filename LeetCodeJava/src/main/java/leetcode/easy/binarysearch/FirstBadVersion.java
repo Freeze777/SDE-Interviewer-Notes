56ed40d1d972ec3fcc5f643b26e8d9039466e7b7
Freeze Francis <freezefrancis@gmail.com>
2022-10-29 13:12:27 +0700

    easy binary search

---
+++ LeetCodeJava/src/main/java/leetcode/easy/binarysearch/FirstBadVersion.java
@@ -0,0 +1,39 @@
+package leetcode.easy.binarysearch;
+
+public class FirstBadVersion {
+    private final int bad;
+
+    private FirstBadVersion(int bad) {
+        this.bad = bad;
+    }
+
+    private boolean isBadVersion(int version) {
+        return version >= bad;
+    }
+
+    public int firstBadVersion(int n) {
+        return firstBadVersion(1, n);
+    }
+
+    private int firstBadVersion(int start, int end) {
+        int mid = (start >> 1) + (end >> 1); // avoids overflows
+        boolean isMidBad = isBadVersion(mid);
+        if (isMidBad) {
+            boolean isPrevBad = isBadVersion(mid - 1);
+            if (isPrevBad) return firstBadVersion(start, mid - 1);
+            else return mid;
+        } else {
+            boolean isNextBad = isBadVersion(mid + 1);
+            if (isNextBad) return mid + 1;
+            else return firstBadVersion(mid + 1, end);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new FirstBadVersion(4).firstBadVersion(5)); //4
+        System.out.println(new FirstBadVersion(1).firstBadVersion(1)); //1
+        System.out.println(new FirstBadVersion(3).firstBadVersion(7)); //3
+        System.out.println(new FirstBadVersion(6).firstBadVersion(7)); //6
+        System.out.println(new FirstBadVersion(1702766719).firstBadVersion(2126753390)); //1702766719
+    }
+}
