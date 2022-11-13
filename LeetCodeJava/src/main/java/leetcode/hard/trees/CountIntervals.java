a45cecf17a7d13f68fc6e608de45bdd316c014f7
freezefrancis <freezefrancis@gmail.com>
2022-11-13 22:34:17 +0700

    AC -AVL tree

--- LeetCodeJava/src/main/java/leetcode/hard/trees/CountIntervals.java
+++ LeetCodeJava/src/main/java/leetcode/hard/trees/CountIntervals.java
@@ -1,68 +1,96 @@
 package leetcode.hard.trees;
 
 class CountIntervals {
-
-    private TreeNode root;
-
-    private static class TreeNode {
-        int start, end, count;
-        TreeNode left, right;
-
-        public TreeNode(int start, int end) {
-            this.start = start;
-            this.end = end;
-            this.count = end - start + 1;
-        }
-
-        @Override
-        public String toString() {
-            return "TreeNode{" + "start=" + start + ", end=" + end + ", count=" + count + "}";
-        }
-    }
+    private AvlTreeNode _root;
 
     public CountIntervals() {
     }
 
-    public void printTree(TreeNode root) {
+    private static class AvlTreeNode {
+        int start, end, height, balanceFactor, range, subtreeRange;
+        AvlTreeNode left, right;
+
+        public AvlTreeNode(int start, int end) {
+            this.start = start;
+            this.end = end;
+            this.subtreeRange = this.range = end - start + 1;
+            this.balanceFactor = this.height = 0;
+        }
+
+        @Override
+        public String toString() {
+            return "TreeNode{" + "start=" + start + ", end=" + end + ", subtreeRange=" + subtreeRange + "}";
+        }
+    }
+
+    private AvlTreeNode balance(AvlTreeNode node) {
+        //left heavy
+        if (node.balanceFactor == -2) {
+            if (node.left.balanceFactor > 0) {
+                node.left = leftRotate(node.left);
+            }
+            return rightRotate(node);
+        }
+        //right heavy
+        else if (node.balanceFactor == 2) {
+            if (node.right.balanceFactor < 0) {
+                node.right = rightRotate(node.right);
+            }
+            return leftRotate(node);
+        }
+        return node;
+    }
+
+    private AvlTreeNode rightRotate(AvlTreeNode a) {
+        AvlTreeNode b = a.left;
+        a.left = b.right;
+        b.right = a;
+        update(a);
+        update(b);
+        return b;
+    }
+
+    private AvlTreeNode leftRotate(AvlTreeNode b) {
+        AvlTreeNode a = b.right;
+        b.right = a.left;
+        a.left = b;
+        update(b);
+        update(a);
+        return a;
+    }
+
+    public void printTree(AvlTreeNode root) {
         if (root == null) return;
         printTree(root.left);
         System.out.println(root);
         printTree(root.right);
     }
 
-    private void computeCount(TreeNode root) {
-        if (root == null) return;
-        root.count = root.end - root.start + 1;
-        if (root.left != null) root.count += root.left.count;
-        if (root.right != null) root.count += root.right.count;
+    private void update(AvlTreeNode node) {
+        int leftHeight = node.left == null ? -1 : node.left.height;
+        int rightHeight = node.right == null ? -1 : node.right.height;
+        node.height = 1 + Math.max(leftHeight, rightHeight);
+        node.balanceFactor = rightHeight - leftHeight;
+        int totalLeftRange = node.left == null ? 0 : node.left.subtreeRange;
+        int totalRightRange = node.right == null ? 0 : node.right.subtreeRange;
+        node.subtreeRange = node.range + totalLeftRange + totalRightRange;
     }
 
-    private TreeNode insertInterval(TreeNode root, int start, int end) {
-        if (root == null) return new TreeNode(start, end);
-        if (start >= root.start && end <= root.end) return root;
-        if (end < root.start) {
-            root.left = insertInterval(root.left, start, end);
-            computeCount(root);
-            return root;
-        }
-        if (start > root.end) {
-            root.right = insertInterval(root.right, start, end);
-            computeCount(root);
-            return root;
-        }
-        //cut intervals
-        if (start < root.start) root.left = insertInterval(root.left, start, root.start - 1);
-        if (end > root.end) root.right = insertInterval(root.right, root.end + 1, end);
-        computeCount(root);
-        return root;
+    private AvlTreeNode insert(AvlTreeNode node, int start, int end) {
+        if (start > end) return node;
+        if (node == null) return new AvlTreeNode(start, end);
+        node.left = insert(node.left, start, Math.min(node.start - 1, end));
+        node.right = insert(node.right, Math.max(node.end + 1, start), end);
+        update(node);
+        return balance(node);
     }
 
     public void add(int start, int end) {
-        root = insertInterval(root, start, end);
+        _root = insert(_root, start, end);
     }
 
     public int count() {
-        return root == null ? 0 : root.count;
+        return _root == null ? 0 : _root.subtreeRange;
     }
 
     public static void main(String[] args) {
@@ -93,11 +121,5 @@ class CountIntervals {
         ci2.add(30, 40);
         ci2.add(60, 70);
         System.out.println(ci2.count());//38
-        //ci2.printTree(ci2._root);
     }
-    /*
-    ["CountIntervals","add","count","add","count","add","count","add","count","add","add","count","add","count","count","add","count","count","count","count","add","count","count","count","add","count","count","count","count","add","add","add","add","add","count","add","count","count","count","add","add","count","count","count","count","count","count","add","count","count","add","add","add","count","add","count","add","add","count","count","count","add","count","count","add","add","count","add","count","count","count","count"]
-    [[],[25,159],[],[343,892],[],[565,976],[],[333,368],[],[424,666],[800,858],[],[249,746],[],[],[759,881],[],[],[],[],[447,668],[],[],[],[489,525],[],[],[],[],[686,833],[579,763],[453,770],[329,411],[229,860],[],[384,491],[],[],[],[882,924],[451,592],[],[],[],[],[],[],[691,820],[],[],[251,641],[921,968],[199,820],[],[781,892],[],[89,477],[142,610],[],[],[],[894,942],[],[],[995,998],[74,830],[],[882,958],[],[],[],[]]
-    */
-
 }
