c6df2625ebabf85e960e806fbe7949366343bb91
freezefrancis <freezefrancis@gmail.com>
2022-11-20 23:46:19 +0700

    easy linkedlist

---
+++ LeetCodeKotlin/src/main/kotlin/leetcode/easy/linkedlist/LinkedListCycle.kt
@@ -0,0 +1,32 @@
+package leetcode.easy.linkedlist
+
+/**
+ * 141. Linked List Cycle
+ * https://leetcode.com/problems/linked-list-cycle/
+ */
+class LinkedListCycle {
+    class ListNode(var `val`: Int) {
+        var next: ListNode? = null
+    }
+
+    fun hasCycle(head: ListNode?): Boolean {
+        var tmp = head
+        val visited = HashSet<Int>()
+        while (tmp != null) {
+            if (visited.contains(tmp.hashCode())) return true
+            visited.add(tmp.hashCode())
+            tmp = tmp.next
+        }
+        return false
+    }
+}
+
+fun main() {
+    val s = LinkedListCycle()
+    val head = LinkedListCycle.ListNode(3)
+    head.next = LinkedListCycle.ListNode(2)
+    head.next!!.next = LinkedListCycle.ListNode(0)
+    head.next!!.next!!.next = LinkedListCycle.ListNode(-4)
+    head.next!!.next!!.next!!.next = head.next
+    println(s.hasCycle(head)) // true
+}
