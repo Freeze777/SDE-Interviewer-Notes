be87353846952f5a62d9c679c3a246494c422e4a
ffrancis <freeze.francis@agoda.com>
2022-11-21 13:33:33 +0700

    nearest exist - AC

--- LeetCodeJava/src/main/java/leetcode/medium/graph/NearestExitMaze.java
+++ LeetCodeJava/src/main/java/leetcode/medium/graph/NearestExitMaze.java
@@ -4,43 +4,46 @@ import java.awt.Point;
 import java.util.*;
 import java.util.stream.Collectors;
 
+/**
+ * <a href="https://leetcode.com/problems/nearest-exit-from-entrance-in-maze/">https://leetcode.com/problems/nearest-exit-from-entrance-in-maze/</a>
+ */
 public class NearestExitMaze {
 
-    private static class Node {
-        Point point;
+    private static class MazeCell {
+        Point cell;
         int depth;
 
-        public Node(Point point, int depth) {
-            this.point = point;
+        public MazeCell(Point cell, int depth) {
+            this.cell = cell;
             this.depth = depth;
         }
     }
 
     private final static int[][] dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
 
-    private static List<Node> nextNodes(Node node, char[][] maze, Set<Point> visited) {
+    private static List<MazeCell> nextNodes(MazeCell mazeCell, char[][] maze) {
         return Arrays.stream(dirs)
-                .map(dir -> new Point(node.point.x + dir[0], node.point.y + dir[1]))
+                .map(dir -> new Point(mazeCell.cell.x + dir[0], mazeCell.cell.y + dir[1]))
                 .filter(point -> point.x >= 0 && point.x < maze.length && point.y >= 0
-                        && point.y < maze[0].length && maze[point.x][point.y] == '.' && !visited.contains(point))
-                .map(point -> new Node(point, node.depth + 1))
+                        && point.y < maze[0].length && maze[point.x][point.y] == '.')
+                .map(point -> new MazeCell(point, mazeCell.depth + 1))
                 .collect(Collectors.toList());
     }
 
     public int nearestExit(char[][] maze, int[] entrance) {
-        Queue<Node> queue = new LinkedList<>();
-        Set<Point> visited = new HashSet<>();
+        Queue<MazeCell> queue = new LinkedList<>();
         Point start = new Point(entrance[0], entrance[1]);
-        queue.add(new Node(start, 0));
+        queue.add(new MazeCell(start, 0));
+        maze[start.x][start.y] = '+';
         while (!queue.isEmpty()) {
-            Node node = queue.poll();
-            visited.add(node.point);
-            if (!node.point.equals(start) && (node.point.x == 0 || node.point.x == maze.length - 1 || node.point.y == 0
-                    || node.point.y == maze[0].length - 1)) {
-                return node.depth;
+            MazeCell mazeCell = queue.poll();
+            if (!mazeCell.cell.equals(start) && (mazeCell.cell.x == 0 || mazeCell.cell.x == maze.length - 1 || mazeCell.cell.y == 0
+                    || mazeCell.cell.y == maze[0].length - 1)) {
+                return mazeCell.depth;
             }
-
-            queue.addAll(nextNodes(node, maze, visited));
+            List<MazeCell> nextMazeCells = nextNodes(mazeCell, maze);
+            queue.addAll(nextMazeCells);
+            nextMazeCells.forEach(nextMazeCell -> maze[nextMazeCell.cell.x][nextMazeCell.cell.y] = '+'); // avoids TLE
         }
         return -1;
     }
@@ -56,10 +59,14 @@ public class NearestExitMaze {
 
         arr = new char[][]{{'+', '.', '+'}, {'.', '.', '.'}, {'+', '.', '+'}};
         entrance = new int[]{1, 0};
-        System.out.println(new NearestExitMaze().nearestExit(arr, entrance));//1
+        System.out.println(new NearestExitMaze().nearestExit(arr, entrance));//2
 
         arr = new char[][]{{'+', '.', '+'}, {'.', '.', '.'}, {'+', '.', '+'}};
         entrance = new int[]{1, 2};
-        System.out.println(new NearestExitMaze().nearestExit(arr, entrance));//-1
+        System.out.println(new NearestExitMaze().nearestExit(arr, entrance));//2
+
+        arr = new char[][]{{'+', '.', '+'}, {'.', '.', '.'}, {'+', '.', '+'}};
+        entrance = new int[]{0, 0};
+        System.out.println(new NearestExitMaze().nearestExit(arr, entrance));//1
     }
 }
