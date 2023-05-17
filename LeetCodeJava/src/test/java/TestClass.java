4d090da097be4723ceb79b3100e5765777163357
ffrancis <freeze.francis@agoda.com>
2023-05-17 12:07:22 +0700

    junit test class

---
+++ LeetCodeJava/src/test/java/TestClass.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * <a href="https://whimsical.com/system-design-interviews-UHCZM4RoLZgxqfifsb56tv"> White Board</a>
+ */
+public class TestClass {
+
+    public int add(int x, int y) {
+        return x + y;
+    }
+
+    @Test
+    public void test() {
+        assertEquals(add(2, 3), 5);
+    }
+}
