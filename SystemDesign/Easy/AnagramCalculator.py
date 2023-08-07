c56b6993a92e49d7ca595cb206178226242a7d60
ffrancis <freeze.francis@agoda.com>
2023-08-07 11:19:44 +0700

    minor

--- SystemDesign/Easy/AnagramCalculator.py
+++ SystemDesign/Easy/AnagramCalculator.py
@@ -7,3 +7,17 @@ MultiThreadedAnagram.py
 Design doc:
 https://whimsical.com/anagramservice-82Xa6ZibRcMWXV1hxcGiCo
 """
+
+
+"""
+An anagram of a string is another string that contains the same characters, only the order of characters can be different.
+NOTE : Assume english language alphabets in lowercase for now.
+For example,
+1. "abcd" & "dabc"  - YES
+2. "abccc" & "abc"  - NO
+3. "xyz" & "abc"    - NO
+4. "silent" & "listen" - YES
+
+Design a system that takes in 2 files from a user and computes if they are anagrams or not. 
+Scale : 100k requests per day and 50 GB max size per file.
+"""
