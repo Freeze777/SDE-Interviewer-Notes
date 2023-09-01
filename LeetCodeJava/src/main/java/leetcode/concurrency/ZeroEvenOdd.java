ae3a7531b9cd330223631aa3d64b4a6518914fde
ffrancis <freeze.francis@agoda.com>
2023-09-01 18:53:16 +0700

    ZeroEvenOdd medium concurrency

---
+++ LeetCodeJava/src/main/java/leetcode/concurrency/ZeroEvenOdd.java
@@ -0,0 +1,81 @@
+package leetcode.concurrency;
+
+import java.util.concurrent.Semaphore;
+
+/**
+ * <a href="https://leetcode.com/problems/print-zero-even-odd/">1116. Print Zero Even Odd</a>
+ */
+public class ZeroEvenOdd {
+
+    private final Semaphore zero = new Semaphore(1);
+    private final Semaphore even = new Semaphore(0);
+    private final Semaphore odd = new Semaphore(0);
+    final int n;
+
+    public ZeroEvenOdd(int n) {
+        this.n = n;
+    }
+
+    public void zero(IntConsumer printNumber) throws InterruptedException {
+        for (int i = 0; i < n; i++) {
+            zero.acquire();
+            printNumber.accept(0);
+            if (i % 2 == 1) even.release();
+            else odd.release();
+        }
+    }
+
+    public void even(IntConsumer printNumber) throws InterruptedException {
+        for (int i = 2; i <= n; i += 2) {
+            even.acquire();
+            printNumber.accept(i);
+            zero.release();
+        }
+    }
+
+    public void odd(IntConsumer printNumber) throws InterruptedException {
+        for (int i = 1; i <= n; i += 2) {
+            odd.acquire();
+            printNumber.accept(i);
+            zero.release();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        execute(new ZeroEvenOdd(5));
+        System.out.println();
+        execute(new ZeroEvenOdd(10));
+    }
+
+    private static void execute(ZeroEvenOdd zeroEvenOdd) throws InterruptedException {
+        new Thread(() -> {
+            try {
+                zeroEvenOdd.zero(new IntConsumer());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+        new Thread(() -> {
+            try {
+                zeroEvenOdd.even(new IntConsumer());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+        new Thread(() -> {
+            try {
+                zeroEvenOdd.odd(new IntConsumer());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+        Thread.sleep(100);
+    }
+}
+
+class IntConsumer {
+    public void accept(int x) {
+        System.out.println(x);
+    }
+}
+
